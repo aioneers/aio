0ad93d316b5228badc7021f2aaecbd665ff69ba6
maryambahrami <maryam.bahrami@aioneers.com>
2021-02-11 12:09:02 +0330

    Finalize read and write

--- aio/aio/read_and_write.py
+++ aio/aio/read_and_write.py
@@ -1,31 +1,36 @@
 # Author: Maryam
 
 import pandas as pd
-import numpy as np
+import os
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
-from functools import reduce
+import warnings
 
 
-def _write(data, file_name, file_location=None, write_directory=None, to='both', verbose=False):
+def _write(data,
+           file_name,
+           file_location=None,
+           write_directory=None,
+           to='both',
+           verbose=False):
     """
     Save dataframe to the directory as parquet, csv or both
 
     Parameters
     ----------
     data: DataFrame
-        The DataFrame to be written.
+        The DataFrame to be written (saved).
     file_name: str
-        The name of a data file
-    file_location: str
+        The name of a data file (e.g: 'data.csv').
+    file_location: str or path object
         The path of a data file location (folder)
-    write_directory: str
-        The desired path to save the new file
+    write_directory: str or path object
+        The desired path to save the new file (folder)
     to: {'csv', 'parquet', 'both'}, optional
         The format to save the data. Default is 'both'.
     verbose: bool, default False
-        Print out when writing the file
+        Informs when writing the file is successful.
 
     Raises
     -------
@@ -35,7 +40,7 @@ def _write(data, file_name, file_location=None, write_directory=None, to='both',
         # save as csv
         csv_file_name = 'py_' + Path(file_name).stem + '.csv'
         if (file_location is None) and (write_directory is None):
-            data.to_csv(csv_file_name, index=False)
+            data.to_csv(os.path.abspath(csv_file_name), index=False)
         elif write_directory is None:
             data.to_csv(join(file_location, csv_file_name), index=False)
         else:
@@ -45,7 +50,7 @@ def _write(data, file_name, file_location=None, write_directory=None, to='both',
     if to == 'parquet' or to == 'both':
         parquet_file_name = 'py_' + Path(file_name).stem + '.parquet'
         if (file_location is None) and (write_directory is None):
-            data.to_csv(parquet_file_name, index=False)
+            data.to_csv(os.path.abspath(parquet_file_name), index=False)
         elif write_directory is None:
             data.to_csv(join(file_location, parquet_file_name), index=False)
         else:
@@ -55,7 +60,11 @@ def _write(data, file_name, file_location=None, write_directory=None, to='both',
             print("{} is saved to the directory".format(parquet_file_name))
 
 
-def read_and_write(file_name, file_location=None, write_directory=None, to='both', verbose=False):
+def read_and_write(file_name,
+                   file_location=None,
+                   write_directory=None,
+                   to='both',
+                   verbose=False):
     """
     Read CSV or Excel files by removing bad lines and save them
     as parquet, csv or both
@@ -63,49 +72,54 @@ def read_and_write(file_name, file_location=None, write_directory=None, to='both
     Parameters
     ----------
     file_name: str
-        The name of a data file
-    file_location: str
+        The name of a data file (e.g: 'data.csv').
+    file_location: str or path object
         The path of a data file location (folder)
-    write_directory: str
-        The desired path to save the new file
+    write_directory: str or path object
+        The desired path to save the new file (folder)
     to: {'csv', 'parquet', 'both'}, optional
         The format to save the data. Default is 'both'.
     verbose: bool, default False
-        Print out when writing the file
+        Informs when writing the file is successful.
 
     Raises
     -------
     Error when the file name does not contain csv or xls.
     """
 
-    # Create file path
-    if file_location is not None:
-        f_path = join(file_location, file_name)
+    if not _check_file(file_name):
+        if verbose:
+            warnings.warn("Warning.............{} is not valid or"
+                          "it's been already created".format(file_name))
     else:
-        f_path = file_name
+        # Create file path
+        if file_location is not None:
+            f_path = join(file_location, file_name)
+        else:
+            f_path = os.path.abspath(file_name)
 
-    # try to read the file
-    try:
-        if 'csv' in file_name:
-            df = pd.read_csv(f_path, error_bad_lines=False, engine="python", dtype=str)
-            # Save to directory
-            _write(df,
-                   file_name,
-                   file_location=file_location,
-                   write_directory=write_directory,
-                   to=to,
-                   verbose=verbose)
-        elif "xls" in file_name.lower():
-            df = pd.read_excel(f_path, dtype=str)
-            # Save to directory
-            _write(df,
-                   file_name,
-                   file_location=file_location,
-                   write_directory=write_directory,
-                   to=to,
-                   verbose=verbose)
-    except Exception as e:
-        print("Could not read {}, because {}".format(file_name, e))
+        # try to read the file
+        try:
+            if 'csv' in file_name:
+                df = pd.read_csv(f_path, error_bad_lines=False, engine="python", dtype=str)
+                # Save to directory
+                _write(df,
+                       file_name,
+                       file_location=file_location,
+                       write_directory=write_directory,
+                       to=to,
+                       verbose=verbose)
+            elif "xls" in file_name.lower():
+                df = pd.read_excel(f_path, dtype=str)
+                # Save to directory
+                _write(df,
+                       file_name,
+                       file_location=file_location,
+                       write_directory=write_directory,
+                       to=to,
+                       verbose=verbose)
+        except Exception as e:
+            print("Could not read {}, because {}".format(file_name, e))
 
 
 def _check_file(file_name):
@@ -115,7 +129,7 @@ def _check_file(file_name):
     Parameters
     ----------
     file_name: str
-        The name of a data file
+        The name of a data file (e.g: 'data.csv').
 
     Returns
     -------
@@ -123,9 +137,9 @@ def _check_file(file_name):
         True if the file is csv or xls and has not been already created.
     """
     # check if the file has been created or not
-    if ('py_' in file_name) or ('ipynb' in file_name):
+    if ('py_' in str(file_name)) or ('ipynb' in str(file_name)):
         file = False
-    elif ('csv' in file_name) or ("xls" in file_name.lower()):
+    elif ('csv' in str(file_name)) or ("xls" in str(file_name).lower()):
         file = True
     # if it is not csv or xls
     else:
@@ -133,29 +147,124 @@ def _check_file(file_name):
     return file
 
 
-def read_and_write_all(folder_path, to='both', write_directory=None, verbose=False):
+def read_and_write_all(folder_path,
+                       to='both',
+                       write_directory=None,
+                       verbose=False):
     """
-        Read CSV or Excel files in the folder by removing bad lines
-        and save them as parquet, csv or both
+    Read CSV or Excel files in the folder by removing bad lines
+    and save them as parquet, csv or both
 
-        Parameters
-        ----------
-        folder_path: str
-            The path of a data file location (folder)
-        to: {'csv', 'parquet', 'both'}, optional
-            The format to save the data. Default is 'both'.
-        write_directory: str
-            The desired path to save the new file
-        verbose: bool, default False
-            Print out when reading and writing the file
+    Parameters
+    ----------
+    folder_path: str or path object
+        The path of a data file location (folder).
+    to: {'csv', 'parquet', 'both'}, optional
+        The format to save the data. Default is 'both'.
+    write_directory: str or path object
+        The desired path to save the new file (folder).
+    verbose: bool, default False
+        Informs when writing the file is successful.
 
-        Raises
-        -------
-        Error when the file name does not contain csv or xls.
+    Raises
+    ----------
+    Error when the file name does not contain csv or xls.
     """
     for i, f in enumerate(listdir(folder_path)):
         f_path = join(folder_path, f)
         if isfile(f_path) and _check_file(f):
             if verbose:
                 print("{}: Reading {} ...".format(i, f))
-            read_and_write(f, folder_path, to=to, verbose=verbose)
+            read_and_write(f,
+                           folder_path,
+                           write_directory,
+                           to=to,
+                           verbose=verbose)
+
+
+def read_and_concat(list_of_files,
+                    file_location=None,
+                    write_directory=None,
+                    parquet=True,
+                    by_row=True,
+                    save_by=None):
+    """
+    Read a list of parquet or csv file and concatenate them by row
+    or by column. The file is written in the directory under the
+    'save_by' name.
+
+    Parameters
+    ----------
+    list_of_files: list of str
+        list of dataframe names (e.g: ['data_0.csv', 'data_1.csv']).
+    file_location: str or path object
+        The path of a data file location (folder).
+    write_directory: str or path object
+        The desired path to save the new file (folder).
+    parquet: bool, optional. Default True
+        If file formats are parquet or csv (False)
+    by_row: bool. Default True
+        If concatenate by row or by column (False)
+    save_by: str, optional. Default None
+        If not None, the concatenate dataframe is written to the
+        file_location under this name.
+
+    Returns
+    -------
+    concat_output: DataFrame
+        The concatenated dataframe
+        optional: Save the concatenated dataframe to the given directory
+    """
+    if file_location is not None:
+        list_of_files_path = [join(file_location, f) for f in list_of_files]
+    else:
+        list_of_files_path = [os.path.abspath(f) for f in list_of_files]
+    lists_of_dfs = []
+    if parquet:
+        for file in list_of_files_path:
+            lists_of_dfs.append(pd.read_parquet(file))
+    else:
+        for file in list_of_files_path:
+            lists_of_dfs.append(pd.read_csv(file, dtype=str))
+    if by_row:
+        concat_output = pd.concat(lists_of_dfs, axis=0)
+    else:
+        concat_output = pd.concat(lists_of_dfs, axis=1)
+    if save_by is not None:
+        if parquet:
+            to = 'parquet'
+        else:
+            to = 'csv'
+        _write(concat_output,
+               save_by,
+               file_location,
+               write_directory,
+               to=to,
+               verbose=False)
+    return concat_output
+
+
+def read_all_sheets(excel_file,
+                    file_location=None):
+    """
+    Read an excel file with multiple sheets and concatenate the sheets
+
+    Parameters
+    ----------
+    excel_file: str
+        The name of a excel file with multiple sheets (e.g. 'data.xlsx')
+    file_location: str or path object
+        The path of a data file location (folder)
+
+    Returns
+    -------
+    concat_output: List of DataFrame
+        List of dataframes, each sheet is a dataframe
+    """
+    if file_location is not None:
+        excel_file_path = join(file_location, excel_file)
+    else:
+        excel_file_path = os.path.abspath(excel_file)
+    all_sheets = pd.read_excel(excel_file_path, None, dtype=str)
+    list_of_dfs = list(all_sheets.values())
+    return list_of_dfs
--- aio/doc/source/reference/read_and_write.rst
+++ aio/doc/source/reference/read_and_write.rst
@@ -9,8 +9,12 @@ List of Functions
 .. autosummary::
    read_and_write
    read_and_write_all
+   read_and_concat
+   read_all_sheets
 
 Definition of Functions
 ~~~~~~~~~~~~~~~~~~~~~~~
 .. autofunction:: read_and_write
 .. autofunction:: read_and_write_all
+.. autofunction:: read_and_concat
+.. autofunction:: read_all_sheets
